485ca6a35679113e11b3fed89e84ed2f6ec5dc2b
SoniEx2 <endermoneymod@gmail.com>
2023-01-04 21:44:39 -0300

    Initial release

---
+++ protocol-handler.js
@@ -0,0 +1,40 @@
+(function() {
+	// Based on https://github.com/fedi-to/fedi-to.github.io/blob/main/protocol-handler.md
+	var url = new URL(document.location.href);
+	var target = url.searchParams.get("target");
+	if (
+		target === null
+		||
+		!target.startsWith("web+ap://")
+		||
+		target.startsWith("web+ap:///")
+		||
+		target.startsWith("web+ap://\\")
+	) {
+		// FIXME invalid
+		return;
+	}
+	var ap_id = target.replace(/^web\+ap:\/\//, "https://");
+	var ap_url = new URL(ap_id);
+	if (ap_url.username || ap_url.password) {
+		// FIXME use an open proxy to check these
+		return;
+	}
+	fetch(ap_url, {
+		// use GET instead of HEAD for CORS to work
+		// (yes really, lmao)
+		method: "GET",
+		// FIXME add workaround for authorized fetch
+		headers: {Accept: 'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'},
+		mode: "cors"
+	}).then((response) => {
+		var ct = response.headers.get("Content-Type");
+		if (
+			ct.startsWith("application/activity+json")
+			||
+			ct.startsWith("application/ld+json")
+		) {
+			document.location = ap_url;
+		}
+	})
+})();
